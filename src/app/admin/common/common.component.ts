2617031eaa6cf6557d48cef0fbc22251d0e1438f
Ramesh kumar Adury <naveeneve1@gmail.com>
2020-02-28 09:37:14 +0530

    ok

--- src/app/admin/common/common.component.ts
+++ src/app/admin/common/common.component.ts
@@ -2,10 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { AdminService } from 'src/app/admin.service';
 import * as FileSaver from 'file-saver';
 
+import * as XLSX from 'xlsx'
 
-
-// const EXCEL_TYPE = 'application/vnd.openxmlformatsofficedocument.spreadsheetml.sheet;charset=UTF-8';
-// const EXCEL_EXTENSION = '.xlsx';
+const EXCEL_TYPE = 'application/vnd.openxmlformatsofficedocument.spreadsheetml.sheet;charset=UTF-8';
+const EXCEL_EXTENSION = '.xlsx';
 
 
 
@@ -119,19 +119,19 @@ export class CommonComponent implements OnInit {
 
   }
 
-  // public downloadFile(): void {
-  //   const worksheet: XLSX.WorkSheet = XLSX.utils.json_to_sheet(this.fobject);
-  //   const workbook: XLSX.WorkBook = { Sheets: { 'data': worksheet }, SheetNames:
-  //   ['data'] };
-  //   const excelBuffer: any = XLSX.write(workbook, { bookType: 'xlsx', type:
-  //   'array' });
-  //   this.saveAsExcelFile(excelBuffer, 'excelFileName');
-  //   }
-  //   private saveAsExcelFile(buffer: any, fileName: string): void {
-  //   const data: Blob = new Blob([buffer], {type: EXCEL_TYPE});
-  //   FileSaver.saveAs(data, fileName + '_export_' + new Date().getTime() +
-  //   EXCEL_EXTENSION);
-  //   }
+  public downloadFile(): void {
+    const worksheet: XLSX.WorkSheet = XLSX.utils.json_to_sheet(this.fobject);
+    const workbook: XLSX.WorkBook = { Sheets: { 'data': worksheet }, SheetNames:
+    ['data'] };
+    const excelBuffer: any = XLSX.write(workbook, { bookType: 'xlsx', type:
+    'array' });
+    this.saveAsExcelFile(excelBuffer, 'excelFileName');
+    }
+    private saveAsExcelFile(buffer: any, fileName: string): void {
+    const data: Blob = new Blob([buffer], {type: EXCEL_TYPE});
+    FileSaver.saveAs(data, fileName + '_export_' + new Date().getTime() +
+    EXCEL_EXTENSION);
+    }
     
     downloadPDF(){
       const doc = new jsPDF()
